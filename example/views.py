3f1090bd9b5aa53a72379187e24d68587f8459f5
Duongcntt96 <duong.ht96@gmail.com>
2025-10-12 19:09:19 +0700

    Reset

---
+++ example/views.py
@@ -0,0 +1,18 @@
+# example/views.py
+from datetime import datetime
+
+from django.http import HttpResponse
+
+
+def index(request):
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+            <p>Đặng Bá Dương</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
