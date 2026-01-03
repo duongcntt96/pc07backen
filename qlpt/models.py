d51fa794318b9186b69fc7b89a2f5f2c05122001
duongcntt96 <duong.ht96@gmail.com>
2026-01-03 03:03:52 +0000

    Update

--- qlpt/models.py
+++ qlpt/models.py
@@ -7,8 +7,11 @@ class PhuongTien(models.Model):
     nhan_hieu_sat_xi = models.CharField(max_length=255)
     bien_kiem_soat = models.CharField(max_length=255)
     nguyen_nhan_hu_hong = models.TextField()
-    bien_phap_thuc_hien = models.TextField()
-    ket_qua = models.CharField(max_length=255)
+    bien_phap_thuc_hien = models.TextField(blank=True, null=True)
+    ket_qua = models.CharField(max_length=255,blank=True, null=True)
+    nguoi_quan_ly = models.CharField(max_length=255, blank=True, null=True)
+    de_xuat = models.TextField(blank=True, null=True)
+    du_tru_kinh_phi = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         return self.bien_kiem_soat
