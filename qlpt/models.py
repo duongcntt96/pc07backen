fd7426dd02ac4a63e0908aa8ede53967df1983ad
duongcntt96 <duong.ht96@gmail.com>
2026-01-08 15:36:04 +0700

    Version 1.5

--- qlpt/models.py
+++ qlpt/models.py
@@ -61,6 +61,9 @@ class Danh_muc_kho(models.Model):
     ten = models.CharField(max_length=200)
     parent = models.ForeignKey('self', related_name='children', verbose_name='Đơn vị quản lý cấp trên', on_delete=models.CASCADE, null=True,blank=True)
     active = models.BooleanField(default=True)
+    # Vị trí kho: vĩ độ và kinh độ (nullable)
+    latitude = models.DecimalField(verbose_name='Vĩ độ', max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(verbose_name='Kinh độ', max_digits=9, decimal_places=6, null=True, blank=True)
     create_at = models.DateTimeField(auto_now_add=True)
     class Meta:
         verbose_name = 'Danh mục kho'
@@ -98,6 +101,8 @@ class Phieu_nhap(models.Model):
         verbose_name = 'Phiếu nhập'
         verbose_name_plural = 'Phiếu nhập'
         ordering = ['-thoi_gian']
+    def __str__(self):
+        return "Phiếu số "+self.pk.__str__()
     # def __str__(self):
     #     if self.kho_xuat:
     #         return str(self.thoi_gian) + self.kho_xuat.ten + " => " + self.kho_nhap.ten
@@ -126,6 +131,15 @@ class Chi_tiet_phieu_nhap(models.Model):
         Phieu_nhap, related_name='phuong_tiens', on_delete=models.CASCADE,null=True)
     chung_loai = models.ForeignKey(
         Chung_loai, verbose_name='Chủng loại', on_delete=models.RESTRICT)
+    parent_item = models.ForeignKey(
+        'self', 
+        verbose_name='Kèm theo phương tiện', 
+        on_delete=models.CASCADE, 
+        null=True, 
+        blank=True, 
+        related_name='phuong_tien_kem_theo'
+    )
+
     ten = models.CharField(
         verbose_name='Tên', max_length=500)
     so_luong = models.BigIntegerField(verbose_name='Số lượng')
@@ -142,6 +156,8 @@ class Chi_tiet_phieu_nhap(models.Model):
     class Meta:
         verbose_name = 'Chi tiết phiếu nhập'
         verbose_name_plural = 'Chi tiết phiếu nhập'
+    def __str__(self):
+        return self.ten
 
     def save(self, *args, **kwargs):
         if self.phieu_nhap.kho_xuat:
